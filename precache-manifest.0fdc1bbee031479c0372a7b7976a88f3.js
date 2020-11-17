fa605657f74db71ed43b544e3b69ff1efa6058a8
Graeme <graemster@gmail.com>
2020-11-16 18:56:34 -0600

    Updates

---
+++ precache-manifest.0fdc1bbee031479c0372a7b7976a88f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97bf4164a39771c88a7c004e25fce2d",
+    "url": "/ColorPallettes/index.html"
+  },
+  {
+    "revision": "701109b2ac25dd7fdc9d",
+    "url": "/ColorPallettes/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "521921ac4c07b0eec129",
+    "url": "/ColorPallettes/static/css/main.1ddbe572.chunk.css"
+  },
+  {
+    "revision": "701109b2ac25dd7fdc9d",
+    "url": "/ColorPallettes/static/js/2.14533e78.chunk.js"
+  },
+  {
+    "revision": "521921ac4c07b0eec129",
+    "url": "/ColorPallettes/static/js/main.4764bfe1.chunk.js"
+  },
+  {
+    "revision": "58eeac48ef5f99cd09ea",
+    "url": "/ColorPallettes/static/js/runtime~main.ae4934f3.js"
+  },
+  {
+    "revision": "ef87f1ff471973d8620fd9f1ae79632c",
+    "url": "/ColorPallettes/static/media/bg.ef87f1ff.svg"
+  }
+]);
